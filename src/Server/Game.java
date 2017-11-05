47b49377b17cad410e04086e2daf2c523e82002c
Eugene Baranov <zcbw@mail.ru>
2017-11-05 21:22:28 +0300

    finally works (can't belive it)

--- src/Server/Game.java
+++ src/Server/Game.java
@@ -27,11 +27,13 @@ public class Game implements GameInterface {
         winner = 0;
         game_started = false;
         game_ended = false;
+        current_turn = new Field(0,0);
     }
 
     //сделать ход
     @Override
     public String turn(Field field, boolean current_player) throws RemoteException{
+        current_turn = field;
         boolean you_can_turn = true;
         String result ="";
         if(winner==0) {
@@ -40,6 +42,7 @@ public class Game implements GameInterface {
                     you_can_turn = can_turn();
                 }
                 if (you_can_turn) {
+                    clearMarked();
                     if (find(field)) {            //если у вас доступно 3 хода и ваш ход допустим
                         int state = playing_field[field.getNumeric_field()][field.getWord_field()].getCurrent_state();
                         switch (state) {
@@ -129,19 +132,19 @@ public class Game implements GameInterface {
     }
 
     //проверка доступности хода
-    private boolean find(Field field){
+    private boolean find(Field field) {
 
         //нельзя ходить в уже занятые тобой клетки или убитые
-        int check_state = playing_field[field.getNumeric_field()][field.getWord_field()].getCurrent_state();
-        if(check_state == (player ? O : X) || check_state == XDESTRUCTED || check_state == ODESTRUCTED)
+        int check_state = playing_field[current_turn.getNumeric_field()][current_turn.getWord_field()].getCurrent_state();
+        if (check_state == (player ? O : X) || check_state == XDESTRUCTED || check_state == ODESTRUCTED)
             return false;
 
 
         //первые ходы для каждого игрока
-        if(first_player_first_turn && (field.getNumeric_field()==9 && field.getWord_field() == 0)){
+        if (first_player_first_turn && (current_turn.getNumeric_field() == 9 && current_turn.getWord_field() == 0)) {
             first_player_first_turn = false;
             return true;
-        } else if(second_player_first_turn && (field.getNumeric_field()==0 && field.getWord_field() == 9)){
+        } else if (second_player_first_turn && (current_turn.getNumeric_field() == 0 && current_turn.getWord_field() == 9)) {
             second_player_first_turn = false;
             return true;
         }
@@ -151,7 +154,8 @@ public class Game implements GameInterface {
         //ищем активные X и O
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                if (i == 1 && j == 1) {} else { // проверяем все элементы кроме текущего (то есть всю окружность текущего)
+                if (i == 1 && j == 1) {
+                } else { // проверяем все элементы кроме текущего (то есть всю окружность текущего)
                     tempi = field.getNumeric_field() - 1 + i;
                     tempj = field.getWord_field() - 1 + j;
                     if (tempi < 10 && tempi > -1 && tempj < 10 && tempj > -1) { //если не вышли за пределы игрового поля
@@ -164,19 +168,20 @@ public class Game implements GameInterface {
             }
         }
         //если мы не нашли активных X или O - ищем те убитые X или O, которых ещё нет в маркированном списке
-        Field field1 = new Field(0,0);
+        Field field1 = new Field(0, 0);
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                if (i == 1 && j == 1){} else { // проверяем все элементы кроме текущего (то есть всю окружность текущего)
+                if (i == 1 && j == 1) {
+                } else { // проверяем все элементы кроме текущего (то есть всю окружность текущего)
                     tempi = field.getNumeric_field() - 1 + i;
                     tempj = field.getWord_field() - 1 + j;
                     if (tempi < 10 && tempi > -1 && tempj < 10 && tempj > -1) { //если не вышли за пределы игрового поля
                         if ((playing_field[tempi][tempj].getCurrent_state() == //ищем первый мертвый вирус
-                                (player ?  XDESTRUCTED : ODESTRUCTED)) &&
+                                (player ? XDESTRUCTED : ODESTRUCTED)) &&
                                 marked[tempi][tempj] == false) {
                             marked[tempi][tempj] = true;      //если найден маркируем его
-                            field1.changeField(tempi,tempj);
-                            if(find(field1)) //запускаем из него новый поиск и если нашли X или O
+                            field1.changeField(tempi, tempj);
+                            if (find(field1)) //запускаем из него новый поиск и если нашли X или O
                                 return true;                // возвращаем true (всплытие из рекурсии)
                         }
                     }
@@ -233,6 +238,14 @@ public class Game implements GameInterface {
         System.out.println("Введите ход:");
     }
 
+    private void clearMarked(){
+        for(int i=0; i<10; i++){
+            for(int j=0; j<10; j++){
+                marked[i][j] = false;
+            }
+        }
+    }
+
     //Переменные//
 
     private class PlayingField {
@@ -257,6 +270,7 @@ public class Game implements GameInterface {
     private boolean game_started;
     private boolean game_ended;
     private PlayingField[][] playing_field; // Поле игры
+    private Field current_turn;
     private boolean player; // Текущий игрок
     private boolean [][] marked;  //вспомогательное поле для поиска чейнов
     private int turn_number; // номер хода текущего игрока
